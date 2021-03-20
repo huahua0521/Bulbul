5dcab9709080d51667701127a76afb56357eb202
hua <huahua0521@gmail.com>
2021-03-20 21:25:45 +0800

    Initial Commit

---
+++ Bulbul/BulbulApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulbulApp.swift
+//  Bulbul
+//
+//  Created by huahua on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct BulbulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
